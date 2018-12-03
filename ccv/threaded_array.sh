1394b61d21030ec4c2c52e89798bc69e03252227
Jason Gaudette <jason_gaudette@brown.edu>
2018-12-03 07:47:33 -0500

    updated CCV scripts

--- ccv/threaded_array.sh
+++ ccv/threaded_array.sh
@@ -5,16 +5,20 @@
 
 # Default resources are 1 core with 2.8GB of memory per core.
 
-# Use more cores (8):
+# Use more cores:
 #SBATCH -c 16
 
 # Specify a job name:
 #SBATCH -J main4
 
+# Specify an array
+#SBATCH --array=3-10
+
 # Specify an output file
-#SBATCH -o MyThreadedJob-%j.out
-#SBATCH -e MyThreadedJob-%j.out
+#SBATCH -e ArrayJob-%j.err
+#SBATCH -o ArrayJob-%j.out
 
 # Run a command
-./main4 10000000
+echo "Starting job $SLURM_ARRAY_TASK_ID on $HOSTNAME"
+time ./main4 $SLURM_ARRAY_TASK_ID
 
